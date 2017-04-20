801d7d4a5ac0241fb7f7f192b4f4d9931892de93
hquesnevill <hquesnevill@138.102.77.251>
2017-04-20 18:00:39 +0200

    Fix unitary tests

--- duster/HashDNASeq.h
+++ duster/HashDNASeq.h
@@ -1,7 +1,9 @@
 #ifndef HASHDNASEQ_H
 #define HASHDNASEQ_H
 
+#include <iostream>
 #include <string>
+
 
 struct HashDNASeq // Compute hashing value of a word
 {
@@ -12,6 +14,7 @@ struct HashDNASeq // Compute hashing value of a word
 	HashDNASeq(unsigned w,unsigned p=100): kmer_size(w), hole_period(p)
 	{
 		effectiveKmerSize=0;
+		if(hole_period<2) hole_period=w+1;
 		for(unsigned i=1;i<=kmer_size;i++)
 		{
 			if(i%hole_period!=0)
@@ -26,56 +29,59 @@ struct HashDNASeq // Compute hashing value of a word
 	unsigned hash(const char* p)
 	{
 
-		unsigned h=0;
-		for(unsigned i=1;i<=kmer_size && *p!='\0';i++)
+		unsigned h=0, val_nuc=0;
+		for(unsigned i=0;i<kmer_size && *p!='\0';i++,p++)
 		{
-			if(i%hole_period==0)
-			  {p++;i++; continue;}
-		  h<<=2;
-		  unsigned val_nuc=0;
-		  switch(*p)
-		  {
-			  case 'C':
+			if((i+1)%hole_period==0)
+			{
+				continue;
+			}
+			else
+			{
+			  switch(*p)
 			  {
-				  val_nuc=1;
-				  break;
-			  }
-			  case 'G':
-			  {
-				  val_nuc=2;
-				  break;
-			  }
-			  case 'T':
-			  {
-				  val_nuc=3;
-				  break;
-			  }
-			  case 'c':
-			  {
-				  val_nuc=1;
-				  break;
-			  }
-			  case 'g':
-			  {
-				  val_nuc=2;
-				  break;
-			  }
-			  case 't':
-			  {
-				  val_nuc=3;
-				  break;
-			  }
-			  default:
-			  {
-				val_nuc=0;
-				break;
-			  }
-		  }
 
-		  h|=val_nuc;
-		  p++;
+				  case 'C':
+				  {
+					  val_nuc=1;
+					  break;
+				  }
+				  case 'G':
+				  {
+					  val_nuc=2;
+					  break;
+				  }
+				  case 'T':
+				  {
+					  val_nuc=3;
+					  break;
+				  }
+				  case 'c':
+				  {
+					  val_nuc=1;
+					  break;
+				  }
+				  case 'g':
+				  {
+					  val_nuc=2;
+					  break;
+				  }
+				  case 't':
+				  {
+					  val_nuc=3;
+					  break;
+				  }
+				  default:
+				  {
+					val_nuc=0;
+					break;
+				  }
+			  }
+			  h<<=2;
+			  h|=val_nuc;
+			}
 		}
-	  return h;
+		return h;
 	};
 
 	std::string reverse_hash(unsigned key)
@@ -88,9 +94,9 @@ struct HashDNASeq // Compute hashing value of a word
 		std::string kmer;
 
 		unsigned val=0;
-		for(unsigned i=1;i<=kmer_size;i++)
+		for(unsigned i=0;i<kmer_size;i++)
 		{
-			if(i%hole_period==0)
+			if((kmer_size-i)%hole_period==0)
 				{
 					car='-';
 				}
