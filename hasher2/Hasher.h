70a52b0b75ae038eff1d80e1d6862487f602ac20
Hadi Quesneville <hadi.quesneville@inra.fr>
2018-06-18 12:01:14 +0200

    Simplified version of Hasher

--- hasher2/Hasher.h
+++ hasher2/Hasher.h
@@ -15,8 +15,6 @@
 #include <unordered_map>
 
 #include "RangePair.h"
-#include "FragAlign.h"
-#include "FastExtAlign.h"
 
 #include "HashDNASeq.h"
 #include "../duster/Duster.h"
@@ -31,27 +29,19 @@ class Hasher : public Duster
 			    unsigned start, unsigned end, unsigned numseq, bool repeat,
 				std::vector< Diag >& diag_map, std::vector< Diag >& diag_map_comp);
 	void diagSearch(const SDGBioSeq& sequence, std::vector< Diag >& diag_map, std::vector< Diag >& diag_map_comp,
-	  		unsigned connect_dist, unsigned kmer_size, unsigned min_frag_size, unsigned verbose);
+	  		unsigned connect_dist, unsigned kmer_size, unsigned min_frag_size,  unsigned verbose);
 
-	int extend_len;
 
 	SDGBioSeqDB subject_db;
+	std::list< RangePair > frag;
 
-	typedef std::map<Key, std::list<RangePair> > MapAlign;
-	typedef std::map<Key, std::list<RangePairSet> > MapPath;
-	MapAlign map_align;
-	MapPath map_path;
 
 
  public:
 
-  Hasher(unsigned w=10, unsigned msk=100, unsigned bw=2, unsigned wd=1, unsigned fd=1, unsigned minsize=20,unsigned step=1,
-		  int ext_len=-1):
-			  Duster(w,msk,bw,wd,fd,minsize,step),
-			  extend_len(ext_len)
-    {
-      if(extend_len==-1) extend_len=2*kmer_size;
-    };
+  Hasher(unsigned w=10, unsigned msk=100, unsigned bw=2, unsigned wd=1, unsigned fd=1, unsigned minsize=20,unsigned step=1):
+			  Duster(w,msk,bw,wd,fd,minsize,step)
+    {};
   void load(const SDGString& filenameS, unsigned kmer_size, unsigned kmask, unsigned bkmer_size, unsigned mkmer_size, double count_cutoff, double diversity_cutoff,
 		  unsigned min_count, bool & valid_idx_file)
 	  {
@@ -60,18 +50,8 @@ class Hasher : public Duster
 	  };
   void search(const SDGBioSeq& sequence, unsigned start, unsigned end, unsigned numseq, unsigned connect_dist, unsigned min_frag_size,
 		  bool repeat, unsigned verbose);
-  void fragAlign(double mism, double gopen,
-		 double gext, unsigned over, bool join, unsigned verbose);
-  void write(const SDGBioSeq& sequence, unsigned min_size=6,std::ostream& out=std::cout);
-  void write_align(const SDGBioSeq& sequence, unsigned min_size,std::ostream& out);
-  void print_frag(const SDGBioSeq& sequence, std::ostream& out=std::cout);
-  void print(const SDGBioSeq& sequence, unsigned min_size=6,std::ostream& out=std::cout);
-  void extend(const SDGBioSeq& sequence, const SDGBioSeq& comp_sequence, unsigned min_size=6, unsigned verbose=0);
+  void write_align(const SDGBioSeq& sequence, std::ostream& out);
 
-  struct comp {
-    int operator () (const std::pair<unsigned,RangePair> & a, const std::pair<unsigned,RangePair> & b)
-    {return a.first < b.first;};
-  };
 };
 
 
