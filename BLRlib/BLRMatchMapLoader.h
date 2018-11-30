68be2335c6dad8d509554b3719afde1f61938ef4
Hadi Quesneville <hadi.quesneville@versailles.inra.fr>
2018-11-30 15:52:27 +0100

    Refactor loader and add check file format

--- BLRlib/BLRMatchMapLoader.h
+++ BLRlib/BLRMatchMapLoader.h
@@ -17,10 +17,18 @@
 class BLRMatchMapLoader
 {
 	public:
-		void readAlign(BLRMatchMap& blrmm, std::istringstream& input_align, int verbose=0);
-  		void loadAlign(BLRMatchMap& blrmm, SDGString filename, int verbose=0);
-		void readPath(BLRMatchMap& blrmm, std::istream& input_path, int verbose=0);
-  		void loadPath(BLRMatchMap& blrmm, SDGString filename, int verbose=0);
+	void readAlign(BLRMatchMap& blrmm, std::istream& input_align, int verbose=0);
+	void loadAlign(BLRMatchMap& blrmm, SDGString filename, int verbose=0){
+		std::ifstream input_align(filename);
+		readAlign(blrmm, input_align, verbose);
+	};
+
+	void readPath(BLRMatchMap& blrmm, std::istream& input_path, int verbose=0);
+	void loadPath(BLRMatchMap& blrmm, SDGString filename, int verbose=0){
+		std::ifstream input_path(filename);
+		readPath(blrmm, input_path, verbose);
+	};
+	
 				
 };
 
