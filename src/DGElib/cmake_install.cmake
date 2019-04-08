02c4a15f428baebafa71ed66e520bec451cf3d4f
Hadi Quesneville <hadi.quesneville@inra.fr>
2019-04-08 15:17:25 +0200

    Merge branch 'master' of urgi-git.versailles.inra.fr:hadi.quesneville/TE_finder
    # Conflicts:
    #	.gitignore
    #	INSTALL
    #	LICENSE
    #	README.md
    #	src/BLRlib/BLRMatchJoin.cpp
    #	src/DGElib/ACG.cpp
    #	src/DGElib/ACG.h
    #	src/DGElib/RNG.cpp
    #	src/DGElib/RNG.h
    #	src/SDGlib/testfiles/Test_SDGlib.cpp
    #	src/duster/duster_main.cpp
    #	src/duster/testfiles/test_u.cpp

--- src/DGElib/cmake_install.cmake
+++ src/DGElib/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/hquesnev/src/git/TE_finder/DGElib
+# Install script for directory: /Users/hquesnev/src/git/TE_finder/src/DGElib
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/Users/hquesnev/src/git/TE_finder/bin")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/hquesnev/src/git/TE_finder/DGElib/testfiles/cmake_install.cmake")
+  include("/Users/hquesnev/src/git/TE_finder/src/DGElib/testfiles/cmake_install.cmake")
 
 endif()
 
