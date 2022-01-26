000aaf0056c91602821e8e7a723d4e05964b445f
Hadi Quesneville <hadi.quesneville@inrae.fr>
2022-01-26 09:37:24 +0100

    -fix compilation errors

--- src/SDGlib/FastaIstream.cpp
+++ src/SDGlib/FastaIstream.cpp
@@ -3,39 +3,30 @@
 #include <string>
 #include <sstream>
 
-FastaIstream& FastaIstream::operator>>(BioSeq &p)
-{
-    std::string titre;
-    std::string line;
+FastaIstream &FastaIstream::operator>>(BioSeq &p) {
     std::ostringstream buffstr;
-    bool found=false;
-    if (this->is_open())
-      {
-        while ( *this )
-        {
-            if(peek()=='>')
-            {
-                if(!found)
-                {
-                    if(std::getline(*this,line))
-                    {
-                        titre=line.substr(1);
-                        titre=titre.erase(titre.find_last_not_of("\t\n\v\f\r ") + 1);
+    bool found = false;
+    std::string titre;
+    if (this->is_open()) {
+        while (*this) {
+            std::string line;
+            if (peek() == '>') {
+                if (!found) {
+                    if (std::getline(*this, line)) {
+                        titre = line.substr(1);
+                        titre = titre.erase(titre.find_last_not_of("\t\n\v\f\r ") + 1);
                     }
-                    found=true;
-                }
-                else
+                    found = true;
+                } else
                     break;
+            } else {
+                if (std::getline(*this, line))
+                    buffstr << line;
             }
-            else
-            {
-                if(std::getline(*this,line))
-                    buffstr<<line;
-            }          
-          }         
         }
-
-  p=BioSeq(buffstr.str(),titre);
-  return *this;
+    }
+    p = buffstr.str();
+    p.header=titre;
+    return *this;
 }
 
