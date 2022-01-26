7b41ea02e9e4cf43134a3a4366202693bfd8af84
Hadi Quesneville <hadi.quesneville@inrae.fr>
2022-01-26 17:30:56 +0100

    -fix compilation errors

--- src/SDGlib/FastaIstream.cpp
+++ src/SDGlib/FastaIstream.cpp
@@ -9,24 +9,39 @@ FastaIstream &FastaIstream::operator>>(BioSeq &p) {
     std::string titre;
     std::string line;
     if (this->is_open()) {
-        while (*this) {
-            if (peek() == '>') {
-                if (!found) {
-                    if (std::getline(*this, line)) {
-                        line.erase(0,1);
-                        line.erase(line.find_last_not_of("\t\n\v\f\r ") + 1);
-                        titre=line;
-                        buffstr.str("");
-                        buffstr.clear();
-                    }
-                    found = true;
-                } else
-                    break;
+        while(std::getline(*this, line) || !found) {
+            if (line[0] == '>') {
+                line.erase(0, 1);
+                line.erase(line.find_last_not_of("\t\n\v\f\r ") + 1);
+                titre = line;
+                buffstr.str("");
+                buffstr.clear();
+                found = true;
             } else {
-                if (std::getline(*this, line))
-                    buffstr << line;
+                buffstr << line;
+            }
+            if (peek() == '>' || peek() == EOF) {
+                break;
             }
         }
+//        while (*this) {
+//           if (peek() == '>') {
+//                if (!found) {
+//                    if (std::getline(*this, line)) {
+//                        line.erase(0,1);
+//                        line.erase(line.find_last_not_of("\t\n\v\f\r ") + 1);
+//                        titre=line;
+//                        buffstr.str("");
+//                        buffstr.clear();
+//                    }
+//                    found = true;
+//                } else
+//                    break;
+//            } else {
+//                if (std::getline(*this, line))
+//                    buffstr << line;
+//            }
+//        }
     }
     p = buffstr.str();
     p.header=titre;
